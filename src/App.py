a1a65a058f3471cc29df85d526afc4932b52d2e5
darkgigi <daniel.arriaza.arriaza@gmail.com>
2024-02-26 22:04:27 +0100

    Añadida visualización del estado de los dispositivos

--- src/App.py
+++ src/App.py
@@ -17,7 +17,7 @@ SRV_FRAME = "0000acc0-0000-1000-8000-00805f9b34fb"  # Service propio
 CH_FRAME = "0000acc5-0000-1000-8000-00805f9b34fb"
 CH_CEDA_SEL = "0000acc6-0000-1000-8000-00805f9b34fb"
 
-connections: dict[str : BleakClient] = dict()
+connections: dict[tuple() : BleakClient] = dict()
 
 class App:
     async def exec(self):
@@ -34,45 +34,45 @@ class Window(tk.Tk):
         self.type2 = None
         self.type3 = None
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
-        lfButtons= tk.LabelFrame(self.root, text="Dispositivos: ")
-        lfButtons.grid(column=0, row=0, padx=10, pady=10)
-        self.devices_list = tk.Listbox(lfButtons, selectmode=tk.SINGLE, width=100, height=20)
+        lf_buttons= tk.LabelFrame(self.root, text="Dispositivos: ")
+        lf_buttons.grid(column=0, row=0, padx=10, pady=10)
+        self.devices_list = tk.Listbox(lf_buttons, selectmode=tk.SINGLE, width=100, height=20)
         self.devices_list.grid(column=0, row=1, padx=4, pady=4)
-        #botones
-        button_discover = tk.Button(lfButtons, text="Escanear", command=lambda: self.loop.create_task(self.discover_devices()))
+        
+        button_discover = tk.Button(lf_buttons, text="Escanear", command=lambda: self.loop.create_task(self.discover_devices()))
         button_discover.grid(column=0, row=2, padx=4, pady=4)
 
-        button_connect = tk.Button(lfButtons, text="Conectar", command=lambda: self.loop.create_task(self.connect_device()))
+        button_connect = tk.Button(lf_buttons, text="Conectar", command=lambda: self.loop.create_task(self.connect_device()))
         button_connect.grid(column=0, row=3, padx=4, pady=4)
 
-        button_disconnect = tk.Button(lfButtons, text="Desconectar", command=lambda: self.loop.create_task(self.disconnect_device()))
+        button_disconnect = tk.Button(lf_buttons, text="Desconectar", command=lambda: self.loop.create_task(self.disconnect_device()))
         button_disconnect.grid(column=0, row=4, padx=4, pady=4)
 
-        lfConnection = tk.LabelFrame(self.root, text="Conexiones:")
-        lfConnection.grid(column=1, row=0, padx=5, pady=10)
+        lf_connection = tk.LabelFrame(self.root, text="Conexiones:")
+        lf_connection.grid(column=1, row=0, padx=5, pady=10)
 
-        bConnect = tk.Button(lfConnection,
+        b_connect = tk.Button(lf_connection,
                                 text="Conectar todos",
                                 command=None,
                                 state="disabled")  # ,
 
-        bConnect.grid(column=1, row=3, padx=4, pady=4)
-        bClose = tk.Button(lfConnection,
-                            text="Cerrar todos",
+        b_connect.grid(column=1, row=3, padx=4, pady=4)
+        b_close = tk.Button(lf_connection,
+                            text="Desconectar todos",
                             command= lambda: self.loop.create_task(self.disconnect_all()))
-        bClose.grid(column=1, row=4, padx=4, pady=4)
-        self.txtChest = tk.Label(lfConnection,
+        b_close.grid(column=1, row=4, padx=4, pady=4)
+        self.txt_chest = tk.Label(lf_connection,
                                 background=COLOR_NOT_FOUND,
                                 text="  Tipo 3  ")
-        self.txtChest.grid(column=1, row=2, padx=4, pady=4)
-        self.txtLeg = tk.Label(lfConnection,
+        self.txt_chest.grid(column=1, row=2, padx=4, pady=4)
+        self.txt_leg = tk.Label(lf_connection,
                             background=COLOR_NOT_FOUND,
                             text="  Tipo 1  ")
-        self.txtLeg.grid(column=1, row=0, padx=4, pady=4)
-        self.txtWrist = tk.Label(lfConnection,
+        self.txt_leg.grid(column=1, row=0, padx=4, pady=4)
+        self.txt_wrist = tk.Label(lf_connection,
                                 background=COLOR_NOT_FOUND,
                                 text="  Tipo 2  ")
-        self.txtWrist.grid(column=1, row=1, padx=4, pady=4)
+        self.txt_wrist.grid(column=1, row=1, padx=4, pady=4)
 
     async def show(self):
         while True:
@@ -84,17 +84,30 @@ class Window(tk.Tk):
         self.destroy()
 
     async def discover_devices(self):
+        self.restart_txt()
         discovered = await BleakScanner.discover()
         devices = [(d.name, d.address) for d in discovered]
         self.devices_list.delete(0, tk.END)
         for device in devices:
             if device[0] == 'ChestMonitor':
-                self.type1 = device[1]
+                self.type3 = device[1]
+                self.txt_chest.config(background=COLOR_FOUND)
             if device[0] == 'WristMonitor':
                 self.type2 = device[1]
+                self.txt_wrist.config(background=COLOR_FOUND)
             if device[0] == 'LegMonitor':
-                self.type3 = device[1]
+                self.type1 = device[1]
+                self.txt_leg.config(background=COLOR_FOUND)
             self.devices_list.insert(tk.END, f"{device[0]} {device[1]}")
+        if connections:
+            for name,address in connections:
+                if address == self.type3:
+                    self.txt_chest.config(background=COLOR_CONNECT)
+                if address == self.type2:
+                    self.txt_wrist.config(background=COLOR_CONNECT)
+                if address == self.type1:
+                    self.txt_leg.config(background=COLOR_CONNECT)
+                self.devices_list.insert(tk.END, f"{name} {address}")
 
     async def connect_device(self):
         selected = self.devices_list.curselection()
@@ -102,6 +115,7 @@ class Window(tk.Tk):
             messagebox.showerror("Error de conexión", "Dispositivo no seleccionado.")
             return
         address = self.devices_list.get(selected[0]).split(" ")[1]
+        name = self.devices_list.get(selected[0]).split(" ")[0]
         if address in connections and connections[address].is_connected:
             messagebox.showerror("Error de conexión", "Dispositivo ya conectado.")
             return
@@ -109,7 +123,7 @@ class Window(tk.Tk):
             connection = BleakClient(address)
             await connection.connect()
             self.check_type(address, connection)
-            connections[address] = connection
+            connections[(name,address)] = connection
             messagebox.showinfo("Conexión", "Dispositivo conectado exitosamente.")
         except Exception as e:
             messagebox.showerror("Error de conexión", "Dispositivo no disponible")
@@ -117,32 +131,38 @@ class Window(tk.Tk):
 
     def check_type(self, address, connection: BleakClient):
         if address == self.type1 and connection.is_connected:
-            self.txtLeg.config(background=COLOR_FOUND)
+            self.txt_leg.config(background=COLOR_CONNECT)
         elif address == self.type1 and not connection.is_connected:
-            self.txtLeg.config(background=COLOR_NOT_FOUND)
+            self.txt_leg.config(background=COLOR_FOUND)
         if address == self.type2 and connection.is_connected:
-            self.txtWrist.config(background=COLOR_FOUND)
+            self.txt_wrist.config(background=COLOR_CONNECT)
         elif address == self.type2 and not connection.is_connected:
-            self.txtWrist.config(background=COLOR_NOT_FOUND)
+            self.txt_wrist.config(background=COLOR_FOUND)
         if address == self.type3 and connection.is_connected:
-            self.txtChest.config(background=COLOR_FOUND)
+            self.txt_chest.config(background=COLOR_CONNECT)
         elif address == self.type3 and not connection.is_connected:
-            self.txtChest.config(background=COLOR_NOT_FOUND)
+            self.txt_chest.config(background=COLOR_FOUND)
     
+    def restart_txt(self):
+        self.txt_leg.config(background=COLOR_NOT_FOUND)
+        self.txt_wrist.config(background=COLOR_NOT_FOUND)
+        self.txt_chest.config(background=COLOR_NOT_FOUND)
+
     async def disconnect_device(self):
         selected = self.devices_list.curselection()
         if not selected:
             messagebox.showerror("Error de desconexión", "Dispositivo no seleccionado.")
             return
         address = self.devices_list.get(selected[0]).split(" ")[1]
+        name = self.devices_list.get(selected[0]).split(" ")[0]
         if address not in connections:
             messagebox.showerror("Error de desconexión", "Dispositivo no conectado.")
             return
         try:
-            connection = connections[address]
+            connection = connections[(name,address)]
             await connection.disconnect()
             self.check_type(address, connection)
-            connections.pop(address)
+            connections.pop((name,address))
             messagebox.showinfo("Desconexión", "Dispositivo desconectado correctamente.")
         except Exception as e:
             messagebox.showerror("Error de desconexión", "Dispositivo no disponible")
@@ -155,6 +175,7 @@ class Window(tk.Tk):
         connections.clear()
         messagebox.showinfo("Desconexión", "Todos los dispositivos desconectados correctamente.")
         return
+    
 if __name__ == "__main__":
     install_dependencies()
     asyncio.run(App().exec())
