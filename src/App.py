37177b77d5e1592b0ade6a8107b8ab4a02113edd
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2024-03-09 12:47:39 +0100

    Añadida obtención de datos de los dispositivos BLE

--- src/App.py
+++ src/App.py
@@ -4,6 +4,9 @@ from bleak import *
 import asyncio
 import threading
 import subprocess
+from pylsl import StreamInfo, StreamOutlet
+from utils import *
+import pylsl as lsl
 
 def install_dependencies():
     subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
@@ -29,10 +32,11 @@ class Window(tk.Tk):
     def __init__(self,loop):
         super().__init__()
         self.title("BLEWin")
+        self.resizable(False, False)
         self.loop = loop
-        self.type1 = None
-        self.type2 = None
-        self.type3 = None
+        self.type3 = blemanager()
+        self.type2 = blemanager()
+        self.type1 = blemanager()
         self.protocol("WM_DELETE_WINDOW", self.on_close)
         lf_buttons= tk.LabelFrame(self, text="Dispositivos: ")
         lf_buttons.grid(column=0, row=0, padx=10, pady=10)
@@ -73,42 +77,61 @@ class Window(tk.Tk):
                                 text="  Tipo 2  ")
         self.txt_wrist.grid(column=1, row=1, padx=4, pady=4)
 
+        lf_measurement = tk.LabelFrame(self, text="Medición:")
+        lf_measurement.grid(column=2, row=0, padx=5, pady=10)
+        b_start = tk.Button(lf_measurement,
+                            text="Iniciar",
+                            command=lambda: self.loop.create_task(self.start_measurement()))
+        b_start.grid(column=2, row=0, padx=4, pady=4)
+        b_stop = tk.Button(lf_measurement,
+                            text="Detener",
+                            command = None)
+        b_stop.grid(column=2, row=1, padx=4, pady=4)
+
     async def show(self):
+        """Muestra la ventana y comienza el bucle de eventos."""
+
         while True:
             self.update()
             await asyncio.sleep(.1)
 
     def on_close(self):
+        """Detiene el bucle de eventos y cierra la ventana."""
+
         self.loop.stop()
         super().destroy()
 
     async def discover_devices(self):
+        """Busca dispositivos BLE disponibles y los muestra en la lista de dispositivos."""
+
         self.restart_txt()
         discovered = await BleakScanner.discover()
         devices = [(d.name, d.address) for d in discovered]
         self.devices_list.delete(0, tk.END)
         for device in devices:
             if device[0] == 'ChestMonitor':
-                self.type3 = device[1]
+                self.type3.address = device[1]
                 self.txt_chest.config(background=COLOR_FOUND)
             if device[0] == 'WristMonitor':
-                self.type2 = device[1]
+                self.type2.address = device[1]
                 self.txt_wrist.config(background=COLOR_FOUND)
             if device[0] == 'LegMonitor':
-                self.type1 = device[1]
+                self.type1.address = device[1]
                 self.txt_leg.config(background=COLOR_FOUND)
             self.devices_list.insert(tk.END, f"{device[0]} {device[1]}")
         if connections:
             for name,address in connections:
-                if address == self.type3:
+                if address == self.type3.address:
                     self.txt_chest.config(background=COLOR_CONNECT)
-                if address == self.type2:
+                if address == self.type2.address:
                     self.txt_wrist.config(background=COLOR_CONNECT)
-                if address == self.type1:
+                if address == self.type1.address:
                     self.txt_leg.config(background=COLOR_CONNECT)
                 self.devices_list.insert(tk.END, f"{name} {address}")
 
     async def connect_device(self):
+        """Conecta el dispositivo seleccionado de la lista de dispositivos."""
+
         selected = self.devices_list.curselection()
         if not selected:
             messagebox.showerror("Error de conexión", "Dispositivo no seleccionado.")
@@ -123,31 +146,38 @@ class Window(tk.Tk):
             await connection.connect()
             self.check_type(address, connection)
             connections[(name,address)] = connection
+            self.initialite_blemanager(address)
             messagebox.showinfo("Conexión", "Dispositivo conectado exitosamente.")
         except Exception as e:
             messagebox.showerror("Error de conexión", "Dispositivo no disponible")
 
 
     def check_type(self, address, connection: BleakClient):
-        if address == self.type1 and connection.is_connected:
+        """Actualiza el color de los labels de estado de los dispositivos encontrados y conectados."""
+
+        if address == self.type1.address and connection.is_connected:
             self.txt_leg.config(background=COLOR_CONNECT)
-        elif address == self.type1 and not connection.is_connected:
+        elif address == self.type1.address and not connection.is_connected:
             self.txt_leg.config(background=COLOR_FOUND)
-        if address == self.type2 and connection.is_connected:
+        if  address == self.type2.address and connection.is_connected:
             self.txt_wrist.config(background=COLOR_CONNECT)
-        elif address == self.type2 and not connection.is_connected:
+        elif  address == self.type2.address and not connection.is_connected:
             self.txt_wrist.config(background=COLOR_FOUND)
-        if address == self.type3 and connection.is_connected:
+        if address == self.type3.address and connection.is_connected:
             self.txt_chest.config(background=COLOR_CONNECT)
-        elif address == self.type3 and not connection.is_connected:
+        elif  address == self.type3.address and not connection.is_connected:
             self.txt_chest.config(background=COLOR_FOUND)
     
     def restart_txt(self):
+        """Reinicia el color de los labels de estado de los dispositivos."""
+
         self.txt_leg.config(background=COLOR_NOT_FOUND)
         self.txt_wrist.config(background=COLOR_NOT_FOUND)
         self.txt_chest.config(background=COLOR_NOT_FOUND)
 
     async def disconnect_device(self):
+        """Desconecta el dispositivo seleccionado."""
+
         selected = self.devices_list.curselection()
         if not selected:
             messagebox.showerror("Error de desconexión", "Dispositivo no seleccionado.")
@@ -161,45 +191,220 @@ class Window(tk.Tk):
             connection = connections[(name,address)]
             await connection.disconnect()
             self.check_type(address, connection)
+            self.restart_blemanager(address)
             connections.pop((name,address))
             messagebox.showinfo("Desconexión", "Dispositivo desconectado correctamente.")
         except Exception as e:
             messagebox.showerror("Error de desconexión", "Dispositivo no disponible")
 
     async def disconnect_all(self):
+        """Desconecta todos los dispositivos conectados."""
+
         global connections
         connections_copy = connections.copy()
         for name,address in connections.keys():
             connection = connections[(name,address)]
             await connection.disconnect()
             self.check_type(address, connection)
+            self.restart_blemanager(address)
             connections_copy.pop((name,address))
         connections = connections_copy
         messagebox.showinfo("Desconexión", "Todos los dispositivos desconectados correctamente.")
         return
     
     async def connect_all(self):
+        """Conecta todos los dispositivos encontrados en la lista de dispositivos."""
+
         if not self.type1 and not self.type2 and not self.type3:
             messagebox.showerror("Error de conexión", "No se ha encontrado ningún dispositivo")
             return
         addresses_connected = [address for _,address in connections.keys()]
-        if self.type1 and self.type1 not in addresses_connected:
-            connection = BleakClient(self.type1)
+        if self.type1.address and self.type1.address not in addresses_connected:
+            connection = BleakClient(self.type1.address)
             await connection.connect()
-            self.check_type(self.type1, connection)
-            connections[("LegMonitor",self.type1)] = connection
-        if self.type2 and self.type2 not in addresses_connected:
-            connection = BleakClient(self.type2)
+            self.check_type(self.type1.address, connection)
+            connections[("LegMonitor",self.type1.address)] = connection
+            self.initialite_blemanager(self.type1.address)
+        if self.type2.address and self.type2.address not in addresses_connected:
+            connection = BleakClient(self.type2.address)
             await connection.connect()
-            self.check_type(self.type2, connection)
-            connections[("WristMonitor",self.type2)] = connection
-        if self.type3 and self.type3 not in addresses_connected:
-            connection = BleakClient(self.type3)
+            self.check_type(self.type2.address, connection)
+            connections[("WristMonitor",self.type2.address)] = connection
+            self.initialite_blemanager(self.type2.address)
+        if self.type3.address and self.type3.address not in addresses_connected:
+            connection = BleakClient(self.type3.address)
             await connection.connect()
-            self.check_type(self.type3, connection)
-            connections[("ChestMonitor",self.type3)] = connection
+            self.check_type(self.type3.address, connection)
+            connections[("ChestMonitor",self.type3.address)] = connection
+            self.initialite_blemanager(self.type3.address)
         messagebox.showinfo("Conexión", "Todos los dispositivos conectados correctamente.")
         return
+    
+    def initialite_blemanager(self, address):
+        """Inicializa los valores de los dispositivos BLE que se conectan"""
+
+        if not self.type1 and not self.type2 and not self.type3:
+            messagebox.showerror("Error de conexión", "No se ha encontrado ningún dispositivo")
+            return
+        if not connections:
+            messagebox.showerror("Error de conexión", "No se ha conectado ningún dispositivo")
+            return
+        if self.type1.address == address and (name:=("LegMonitor",self.type1.address)) in connections:
+            connection = connections[name]
+            self.type1.id = 1
+            self.type1.lsl_ta = StreamOutlet(
+                StreamInfo('Nano33IoT_Leg_TA', 'TA', 1, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type1.lsl_acc = StreamOutlet(
+                StreamInfo('Nano33IoT_Leg_ACC', 'ACC', 3, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type1.lsl_gyr = StreamOutlet(
+                StreamInfo('Nano33IoT_Leg_GYR', 'GYR', 3, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type1.lsl_bat = StreamOutlet(
+                StreamInfo('Nano33IoT_Leg_BAT', 'BAT', 1, lsl.IRREGULAR_RATE, 'float32', 'OWN'))
+            print(self.type1)
+        if self.type2.address == address and (name:=("WristMonitor",self.type2.address)) in connections:
+            connection = connections[name]
+            self.type2.eda_config = connection.read_gatt_char(CH_CEDA_SEL)[0]
+            self.type2.id = 2
+
+            self.type2.lsl_eda = StreamOutlet(
+                StreamInfo('Nano33IoT_Wrist_EDA', 'EDA', 1, lsl.IRREGULAR_RATE, 'int16', 'OWN'))
+            self.type2.lsl_eda_config = StreamOutlet(
+                StreamInfo('Nano33IoT_Wrist_EDA_CONFIG', 'EDA_CONFIG', 1, lsl.IRREGULAR_RATE, 'int16', 'OWN'))
+            self.type2.lsl_ta = StreamOutlet(
+                StreamInfo('Nano33IoT_Wrist_TA', 'TA', 1, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type2.lsl_st = StreamOutlet(
+                StreamInfo('Nano33IoT_Wrist_ST', 'ST', 1, lsl.IRREGULAR_RATE, 'float32', 'OWN'))
+            self.type2.lsl_acc = StreamOutlet(
+                StreamInfo('Nano33IoT_Wrist_ACC', 'ACC', 3, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type2.lsl_gyr = StreamOutlet(
+                StreamInfo('Nano33IoT_Wrist_GYR', 'GYR', 3, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type2.lsl_bat = StreamOutlet(
+                StreamInfo('Nano33IoT_Wrist_BAT', 'BAT', 1, lsl.IRREGULAR_RATE, 'float32', 'OWN'))
+        if self.type3.address == address and (name:=("ChestMonitor",self.type3.address)) in connections:
+            connection = connections[name]
+            self.type3.id = 3
+            self.type3.lsl_ecg = StreamOutlet(
+                StreamInfo('Nano33IoT_Chest_ECG', 'ECG', 1, lsl.IRREGULAR_RATE, 'int16', 'OWN'))
+            self.type3.lsl_hr = StreamOutlet(
+                StreamInfo('Nano33IoT_Chest_HR', 'HR', 1, lsl.IRREGULAR_RATE, 'float32', 'OWN'))
+            self.type3.lsl_br = StreamOutlet(
+                StreamInfo('Nano33IoT_Chest_BR', 'BR', 1, lsl.IRREGULAR_RATE, 'int16', 'OWN'))
+            self.type3.lsl_ta = StreamOutlet(
+                StreamInfo('Nano33IoT_Chest_TA', 'TA', 1, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type3.lsl_acc = StreamOutlet(
+                StreamInfo('Nano33IoT_Chest_ACC', 'ACC', 3, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type3.lsl_gyr = StreamOutlet(
+                StreamInfo('Nano33IoT_Chest_GYR', 'GYR', 3, lsl.IRREGULAR_RATE, 'float32', 'LSM6DS3'))
+            self.type3.lsl_bat = StreamOutlet(
+                StreamInfo('Nano33IoT_Chest_BAT', 'BAT', 1, lsl.IRREGULAR_RATE, 'float32', 'OWN'))
+            print(self.type3)
+    
+    def restart_blemanager(self, address):
+        """Reinicia los valores de los dispositivos BLE que se desconectan"""
+
+        connected_addresses = [address for _,address in connections.keys()]
+        if self.type1.address == address and self.type1.address in connected_addresses:
+            self.type1.lsl_ta = None
+            self.type1.lsl_acc = None
+            self.type1.lsl_gyr = None
+            self.type1.lsl_bat = None
+        if self.type2.address == address and self.type2.address in connected_addresses:
+            self.type2.eda_config = None
+            self.type2.lsl_eda = None
+            self.type2.lsl_eda_config = None
+            self.type2.lsl_ta = None
+            self.type2.lsl_st = None
+            self.type2.lsl_acc = None
+            self.type2.lsl_gyr = None
+            self.type2.lsl_bat = None
+        if self.type3.address == address and self.type3.address in connected_addresses:
+            self.type3.lsl_ecg = None
+            self.type3.lsl_hr = None
+            self.type3.lsl_br = None
+            self.type3.lsl_ta = None
+            self.type3.lsl_acc = None
+            self.type3.lsl_gyr = None
+            self.type3.lsl_bat = None
+
+    def getLegData(self, data):
+        """Obtiene los datos de la pierna"""
+
+        if len(data) != 28:
+            print("Error Data 0x01: Wrong length.")
+            return None, None, None, None
+        else:
+            acc = bytearray(data[0:12])
+            gyr = bytearray(data[12:24])
+            bat = bytearray(data[24:26])
+            ta = bytearray(data[26:28])
+            r_acc = accRead(acc)
+            r_gyr = gyrRead(gyr)
+            r_bat = batRead(bat)
+            r_ta = taRead(ta)
+            return r_acc, r_gyr, r_bat, r_ta
+    
+    def getWristData(self,data):
+        """Obtiene los datos de la muñeca"""
+
+        # TODO: probar si se reciben bien los datos de la muñeca
+        if len(data) != 36:
+            print("Error Data 0x01: Wrong length.")
+            return None, None, None, None, None, None, None
+        else:
+            eda = bytearray(data[0:4])
+            acc = bytearray(data[4:16])
+            gyr = bytearray(data[16:28])
+            bat = bytearray(data[28:30])
+            ta = bytearray(data[30:32])
+            st = bytearray(data[32:34])
+            eda_config = bytearray(data[34:36])
+            r_eda = bytearray2uint16list(eda)
+            r_acc = accRead(acc)
+            r_gyr = gyrRead(gyr)
+            r_bat = batRead(bat)
+            r_ta = taRead(ta)
+            r_st = stRead(st)
+            r_eda_config = bytearray2uint16list(eda_config)
+            return r_eda, r_acc, r_gyr, r_bat, r_ta, r_st, r_eda_config
+    
+    def getChestData(self,data):
+        """Obtiene los datos del pecho"""
+
+        # TODO: corregir el error de longitud, el tamaño que recibe es de 72 bytes
+        if len(data) != 64:
+            print("Error Data 0x01: Wrong length.")
+            return None, None, None, None, None, None
+        else:
+            ecg = bytearray(data[0:32])
+            acc = bytearray(data[32:44])
+            gyr = bytearray(data[44:56])
+            br = bytearray(data[56:60])
+            bat = bytearray(data[60:62])
+            ta = bytearray(data[62:64])
+            r_ecg = bytearray2uint16list(ecg)
+            r_acc = accRead(acc)
+            r_gyr = gyrRead(gyr)
+            r_br = bytearray2uint16list(br)
+            r_bat = batRead(bat)
+            r_ta = taRead(ta)
+            return r_ecg, r_acc, r_gyr, r_br, r_bat, r_ta
+
+    # TODO: procesar los datos recibidos de los dispositivos
+    async def start_measurement(self):
+        """Inicia la medición de los dispositivos conectados."""
+        if not connections:
+            messagebox.showerror("Error de medición", "No se ha conectado ningún dispositivo")
+            return
+        connected_addresses = [address for _,address in connections.keys()]
+        if self.type1.address in connected_addresses:
+            data = await connections[("LegMonitor",self.type1.address)].read_gatt_char(CH_FRAME)
+            r_acc, r_gyr, r_bat, r_ta = self.getLegData(data)
+        if self.type2.address in connected_addresses:
+            data = await connections[("WristMonitor",self.type2.address)].read_gatt_char(CH_FRAME)
+            r_eda, r_acc, r_gyr, r_bat, r_ta, r_st, r_eda_config = self.getWristData(data)
+        if self.type3.address in connected_addresses:
+            data = await connections[("ChestMonitor",self.type3.address)].read_gatt_char(CH_FRAME)
+            r_ecg, r_acc, r_gyr, r_br, r_bat, r_ta = self.getChestData(data)
     
 if __name__ == "__main__":
     install_dependencies()
---
+++ src/utils.py
@@ -0,0 +1,123 @@
+def bytearray2uint16(br: bytearray) -> int:
+    """Convierte un bytearray en un entero sin signo de 16 bits"""
+
+    return int.from_bytes(br, byteorder='little', signed=False)
+
+
+def bytearray2uint16list(br: bytearray) -> list:
+    """Convierte un bytearray en una lista de enteros sin signo de 16 bits"""
+
+    mr = list()
+    br2 = len(br)
+    for j in range(0, br2 >> 1):
+        aux = br[2 * j:2 * j + 2]
+        mf = bytearray2uint16(aux)
+        mr.append(mf)
+
+    return mr
+
+
+def bytearray2int16(br: bytearray) -> int:
+    """Convierte un bytearray en un entero de 16 bits"""
+
+    return int.from_bytes(br, byteorder='little', signed=True)
+
+
+def bytearray2int16list(br: bytearray) -> list:
+    """Convierte un bytearray en una lista de enteros de 16 bits"""
+
+    mr = list()
+    br2 = len(br)
+    for j in range(0, br2 >> 1):
+        aux = br[2 * j:2 * j + 2]
+        mf = bytearray2int16(aux)
+        mr.append(mf)
+
+    return mr
+
+
+def accRead(br: bytearray) -> list:
+    """Convierte un bytearray en una lista de aceleraciones en g (float)"""
+
+    mr = bytearray2int16list(br)
+    m_ret = [accint162float32(element) for element in mr]
+    return m_ret
+
+
+def accint162float32(value) -> float:
+    """Convierte un entero de 16 bits en un float de 32 bits"""
+
+    return float(value) * 4.0 / 32768.0
+
+
+def gyrRead(br: bytearray) -> list:
+    """Convierte un bytearray en una lista de velocidades angulares en grados por segundo (float)"""
+
+    mr = bytearray2int16list(br)
+    m_ret = [gyrint162float32(element) for element in mr]
+    return m_ret
+
+
+def gyrint162float32(value) -> float:
+    """Convierte un entero de 16 bits en un float de 32 bits"""
+
+    return float(value) * 2000.0 / 32768.0
+
+
+def batRead(br: bytearray) -> list:
+    """Convierte un bytearray en una lista de niveles de batería en porcentaje (float)"""
+
+    mr = list()
+    br2 = len(br)
+    for j in range(0, br2 >> 1):
+        aux = br[2 * j:2 * j + 2]
+        mf = int.from_bytes(aux, byteorder='little', signed=False)
+        ax = float(mf) * 3.3 / 1023.0
+        ax = 5 * ax / 3
+        mr.append(ax)
+    return mr
+
+
+def stRead(br: bytearray) -> list:
+    """Convierte un bytearray en una lista de temperaturas de la piel en grados Celsius (float)"""
+
+    mr = list()
+    br2 = len(br)
+    for j in range(0, br2 >> 1):
+        aux = br[2 * j:2 * j + 2]
+        mf = int.from_bytes(aux, byteorder='little', signed=False)
+        mr.append(mf)
+    # TODO: conversión de uint16 a ºC
+    m_ret = [float(element) for element in mr]  # * 100.0 / 1024.0 for element in mr]
+    return m_ret
+
+
+def taRead(br: bytearray) -> list:
+    """Convierte un bytearray en una lista de temperaturas ambientales en grados Celsius (float)"""
+    
+    mr = bytearray2uint16list(br)
+    m_ret = [float(element) / 16.0 + 25 for element in mr]
+    return m_ret
+
+class blemanager():
+    def __init__(self):
+        self.id = None
+        self.address = None
+        self.eda_config = None          #Electrodermal activity configuration
+        self.lsl_acc = None             #Accelerometer
+        self.lsl_hr = None              #Heart rate
+        self.lsl_bat = None             #Battery level
+        self.lsl_br = None              #Breathing rate
+        self.lsl_ecg = None             #Electrocardiogram
+        self.lsl_eda = None             #Electrodermal activity
+        self.lsl_gyr = None             #Gyroscope
+        self.lsl_st = None              #Skin temperature
+        self.lsl_ta = None              #Ambient temperature
+        self.lsl_eda_config = None      #Electrodermal activity configuration
+
+    def __str__(self):
+        return (f"blemanager(id={self.id}, address={self.address}" 
+        f", eda_config={self.eda_config}, lsl_acc={self.lsl_acc}, lsl_hr={self.lsl_hr}, lsl_bat={self.lsl_bat}"
+        f", lsl_br={self.lsl_br}, lsl_ecg={self.lsl_ecg}, lsl_eda={self.lsl_eda}, lsl_gyr={self.lsl_gyr}, lsl_st={self.lsl_st}" 
+        f", lsl_ta={self.lsl_ta}, lsl_eda_config={self.lsl_eda_config})")
+    
