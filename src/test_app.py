5467222d461a3cdbac468e7cf0638660d780ad49
Daniel Arriaza Arriaza <113770002+darkgigi@users.noreply.github.com>
2024-03-29 14:14:49 +0100

    Creados tests de gestión de dispositivos

--- src/test_app.py
+++ src/test_app.py
@@ -1,41 +1,73 @@
 import asyncio
-import unittest
-from unittest.mock import patch
+import pytest
 from app import Window
 
-COLOR_NOT_FOUND = "#f26f68"
-COLOR_FOUND = "#44dddd"
-COLOR_CONNECT = "#33cc55"
-COLOR_BUTTON_FOCUS = '#82c2f0'
+COLOR_NOT_FOUND = "#e85a5a"
+COLOR_FOUND = "#82c2f0"
+COLOR_CONNECT = "#54ba32"
+COLOR_BUTTON_FOCUS = '#f0982e'
 
-class TestWindow(unittest.IsolatedAsyncioTestCase):
-    def setUp(self):
+class TestWindow:
+    def setup_method(self, method):
         self.window = Window(asyncio.get_event_loop())
 
+    def teardown_method(self, method):
+        self.window.destroy()
 
+    @pytest.mark.asyncio
     async def test_discover_devices(self):
 
         # Comprobar que no se han encontrado dispositivos
-        self.assertEqual(self.window.txt_chest.cget('background'), COLOR_NOT_FOUND)
-        self.assertEqual(self.window.txt_leg.cget('background'), COLOR_NOT_FOUND)
+        assert self.window.txt_chest.cget('fg_color') == COLOR_NOT_FOUND
+        assert self.window.txt_leg.cget('fg_color') == COLOR_NOT_FOUND
 
         # Llamar al SUT
         await self.window.discover_devices()
 
         # Comprobar que se han encontrado los dispositivos de Tipo 1 y 3
-        devices_names = [self.window.devices_list.get(i) for i in range(self.window.devices_list.size())]
-        self.assertTrue('ChestMonitor 4C:11:AE:91:17:FA' in devices_names)
-        self.assertTrue('LegMonitor 4C:11:AE:8C:81:FA' in devices_names)
+        devices_names = [self.window.radiobuttons[i].cget('text') for i in range(len(self.window.radiobuttons))]
+        assert 'ChestMonitor 4C:11:AE:91:17:FA' in devices_names
+        assert 'LegMonitor 4C:11:AE:8C:81:FA' in devices_names
 
         # Comprobar que los tipos están correctos
-        self.assertEqual(self.window.type3.address, '4C:11:AE:91:17:FA')
-        self.assertEqual(self.window.type3.name, 'ChestMonitor')
-        self.assertEqual(self.window.type1.address, '4C:11:AE:8C:81:FA')
-        self.assertEqual(self.window.type1.name, 'LegMonitor')
+        assert self.window.type3.address == '4C:11:AE:91:17:FA'
+        assert self.window.type3.name == 'ChestMonitor'
+        assert self.window.type1.address == '4C:11:AE:8C:81:FA'
+        assert self.window.type1.name == 'LegMonitor'
 
         # Comprobar que los labels están actualizados
-        self.assertEqual(self.window.txt_chest.cget('background'), COLOR_FOUND)
-        self.assertEqual(self.window.txt_leg.cget('background'), COLOR_FOUND)
+        assert self.window.txt_chest.cget('fg_color') == COLOR_FOUND
+        assert self.window.txt_leg.cget('fg_color') == COLOR_FOUND
+
+    @pytest.mark.asyncio
+    async def test_connect_and_disconnect_device(self):
+        # Comprobar que no se han conectado los dispositivos
+        assert self.window.txt_chest.cget('fg_color') == COLOR_NOT_FOUND
+        assert self.window.txt_leg.cget('fg_color') == COLOR_NOT_FOUND
+
+        # Llamar al SUT
+        await self.window.discover_devices()
+        legMonitor = self.window.type1.name + ' ' + self.window.type1.address
+        chestMonitor = self.window.type3.name + ' ' + self.window.type3.address
+        self.window.radio_var.set(legMonitor)
+        
+        await self.window.connect_device()
+        self.window.radio_var.set(chestMonitor)
+        await self.window.connect_device()
+
+        # Comprobar que los dispositivos están conectados
+        assert self.window.txt_chest.cget('fg_color') == COLOR_CONNECT
+        assert self.window.txt_leg.cget('fg_color') == COLOR_CONNECT
+
+        #Llamar al SUT
+        self.window.radio_var.set(legMonitor)
+        await self.window.disconnect_device()
+        self.window.radio_var.set(chestMonitor)
+        await self.window.disconnect_device()
+
+        # Comprobar que los dispositivos están desconectados
+        assert self.window.txt_chest.cget('fg_color') == COLOR_FOUND
+        assert self.window.txt_leg.cget('fg_color') == COLOR_FOUND
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
